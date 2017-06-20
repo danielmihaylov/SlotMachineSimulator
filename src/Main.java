f19b72aa6e1512e64e488f9b404614ed0112dcd6
danielmihaylov <daniel.a.mihaylov@abv.bg>
2017-06-20 11:48:04 +0300

    Added winning logic

--- src/Main.java
+++ src/Main.java
@@ -15,45 +15,66 @@ public class Main {
         String firstWord = "";
         String secondWord = "";
         String thirdWord = "";
+        char playAgain = 'y';
+        Double totalAmountEntered = 0d;
 
-        userInput = JOptionPane.showInputDialog("Insert money: ");
-        amount = Double.parseDouble(userInput);
+        while (playAgain == 'y'){
+            output = "";
+            userInput = JOptionPane.showInputDialog("Insert money: ");
+            amount = Double.parseDouble(userInput);
+            totalAmountEntered += amount;
 
-        for (int i = 0; i < 3 ; i++) {
-            wordIndex = random.nextInt(6);
+            for (int i = 0; i < 3; i++) {
+                wordIndex = random.nextInt(6);
 
-            switch (wordIndex){
-                case 0:word = "cherries";
-                    break;
-                case 1:word = "oranges";
-                    break;
-                case 2:word = "plums";
-                    break;
-                case 3:word = "bells";
-                    break;
-                case 4:word = "melons";
-                    break;
-                case 5: word = "bars";
-                    break;
-                default:
-                    break;
+                switch (wordIndex) {
+                    case 0:
+                        word = "cherries";
+                        break;
+                    case 1:
+                        word = "oranges";
+                        break;
+                    case 2:
+                        word = "plums";
+                        break;
+                    case 3:
+                        word = "bells";
+                        break;
+                    case 4:
+                        word = "melons";
+                        break;
+                    case 5:
+                        word = "bars";
+                        break;
+                    default:
+                        break;
+                }
+                if (i == 0) {
+                    firstWord = word;
+                } else if (i == 1) {
+                    secondWord = word;
+                } else if (i == 2) {
+                    thirdWord = word;
+                }
             }
-            if (i == 0){
-                firstWord = word;
-            }else if(i == 1){
-                secondWord = word;
-            }else if(i == 2){
-                thirdWord = word;
+
+            output = output + "[" + firstWord + "]" + "     " + "[" + secondWord + "]" + "     " + "[" + thirdWord + "]";
+
+            if ((!firstWord.equals(secondWord)) && (!secondWord.equals(thirdWord)) && (!thirdWord.equals(firstWord))) {
+                output = output + "\n\nYou have won $0 !";
+            } else if ((firstWord.equals(secondWord) && !firstWord.equals(thirdWord)) ||
+                    (firstWord.equals(thirdWord) && !firstWord.equals(secondWord)) ||
+                    (secondWord.equals(thirdWord) && !secondWord.equals(firstWord))){
+                output = output + "\n\nYou have won " + (amount * 2);
+            }else{
+                output = output + "\n\nYou have won " + (amount * 5);
             }
+            playAgain = JOptionPane.showInputDialog(output + "\n\n\nDo you want to play " +
+                    "again?\nPress y for yes, any key for no :").charAt(0);
         }
 
-        output = output + "[" + firstWord + "]" + "     " + "[" + secondWord + "]" + "     " + "[" + thirdWord + "]";
 
-        if ((!firstWord.equals(secondWord)) && (!secondWord.equals(thirdWord)) && (!thirdWord.equals(firstWord))){
-            output = output + "\nYou have won $0 !";
-        }
 
-        JOptionPane.showMessageDialog(null,output);
         System.exit(0);
     }
 }
